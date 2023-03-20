1bc1e2d069c8b4f982a09dcde76831404a73d0f3
mosab2020 <mabumuammar@students.iugaza.edu.ps>
2023-03-21 00:16:32 +0200

    Second Commit

--- app/src/main/java/com/example/assignment02/MainActivity.kt
+++ app/src/main/java/com/example/assignment02/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.example.assignment02
 
-import android.app.AlertDialog
-import android.app.DownloadManager
 import android.app.ProgressDialog
-import android.content.Context
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.example.assignment02.databinding.ActivityMainBinding
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.ktx.storage
 import java.io.File
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var progressDialog: ProgressDialog
+
+    private lateinit var selectedFileRef: StorageReference
 
     lateinit var binding: ActivityMainBinding
 
@@ -40,7 +45,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnUpload.setOnClickListener {
-//
+
             val progressDialog = ProgressDialog(this)
             progressDialog.setTitle("Uploading PDF")
             progressDialog.show()
@@ -64,22 +69,36 @@ class MainActivity : AppCompatActivity() {
             progressDialog.setTitle("Downloading File")
             progressDialog.show()
 
-                ref.downloadUrl
-                .addOnSuccessListener { uri ->
-                    progressDialog.show()
-
-                    val intent = Intent(Intent.ACTION_VIEW)
-                    intent.setDataAndType(uri, "application/pdf")
-                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-                    startActivity(intent)
-                }
-                .addOnFailureListener {
-                    progressDialog.show()
-
-                    Toast.makeText(this, "Download failed", Toast.LENGTH_SHORT).show()
-                }
+//                ref.downloadUrl
+//                .addOnSuccessListener { uri ->
+//                    progressDialog.show()
+//
+//                    val intent = Intent(Intent.ACTION_VIEW)
+//                    intent.setDataAndType(uri, "application/pdf")
+//                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+//                    startActivity(intent)
+//                }
+//                .addOnFailureListener {
+//                    progressDialog.show()
+//
+//                    Toast.makeText(this, "Download failed", Toast.LENGTH_SHORT).show()
+//                }
 
         }
+
+
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Downloading...")
+        progressDialog.setCancelable(false)
+
+        displayFiles()
+
+        binding.listView.setOnItemClickListener { _, _, position, _ ->
+            val fileName = binding.listView.getItemAtPosition(position).toString()
+            downloadFile(fileName)
+        }
+
     }
 
     @Deprecated("Deprecated in Java")
@@ -89,7 +108,56 @@ class MainActivity : AppCompatActivity() {
             pdfPath = data!!.data!!
         }
 
+    }
 
+    private fun displayFiles() {
+        val fileList = mutableListOf<String>()
+        ref.listAll()
+            .addOnSuccessListener { listResult ->
+                for (file in listResult.items) {
+                    if (file.name.endsWith(".pdf")) {
+                        fileList.add(file.name)
+                    }
+                }
+                val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, fileList)
+                binding.listView.adapter = adapter
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(this, "Failed to get file list", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun downloadFile(fileName: String) {
+        selectedFileRef = ref.child("pdf/$fileName")
+        val file = File.createTempFile(fileName.split('.')[0], ".pdf")
+        selectedFileRef.getFile(file)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(this, "Download successful", Toast.LENGTH_SHORT).show()
+                openPDF(file)
+            }
+            .addOnFailureListener {
+                progressDialog.dismiss()
+                Toast.makeText(this, "Download failed", Toast.LENGTH_SHORT).show()
+            }
+            .addOnProgressListener { taskSnapshot ->
+                val progress = (100.0 * taskSnapshot.bytesTransferred / taskSnapshot.totalByteCount)
+                progressDialog.setMessage("Downloading ${progress.toInt()}%")
+            }
+        progressDialog.show()
+    }
+
+    private fun openPDF(file: File) {
+        val target = Intent(Intent.ACTION_VIEW)
+        target.setDataAndType(Uri.fromFile(file), "application/pdf")
+        target.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
+
+        val intent = Intent.createChooser(target, "Open File")
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "No PDF viewer installed", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
